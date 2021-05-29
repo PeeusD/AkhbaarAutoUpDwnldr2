0da5a3003282cc8e2a7cbd430c2b91a142da691f
NoobCoder <72473589+PeeusD@users.noreply.github.com>
2021-05-29 07:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ def schedulling_fun():
                     msg ='<b>' + txt + '\t '+ dwld_link +'</b>'
                     
                      #sending greeting sticker
-                    if "the-hindu" in url[i][0] :
-                        bot.send_sticker(chat_id=chat_id, sticker= "CAACAgUAAxkBAAMlYIUpOSktZSWxgoH0bdsRS_86WCgAAggAA1xd_zlw6TJ98knIFB8E")
+                    #if "the-hindu" in url[i][0] :
+                        #bot.send_sticker(chat_id=chat_id, sticker= "CAACAgUAAxkBAAMlYIUpOSktZSWxgoH0bdsRS_86WCgAAggAA1xd_zlw6TJ98knIFB8E")
                         
                     #sending message
                     bot.send_message(chat_id = chat_id, text = msg  , parse_mode = ParseMode.HTML )
